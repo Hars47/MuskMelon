6ea743a620d13581600fea685e34295fbe6e27c5
Hars47 <saharsha2004@gmail.com>
2024-10-21 11:47:52 +0530

    initial commit

---
+++ drizzle.config.js
@@ -0,0 +1,11 @@
+export default {
+    dialect: "postgresql",
+    schema: "./src/utils/db/schema.ts",
+    out: "./drizzle",
+    dbCredentials: {
+      url: "postgresql://neondb_owner:rqjIphOtV09P@ep-delicate-dew-a1fxfm3k.ap-southeast-1.aws.neon.tech/WMA?sslmode=require",
+      connectionString:
+        "postgresql://neondb_owner:rqjIphOtV09P@ep-delicate-dew-a1fxfm3k.ap-southeast-1.aws.neon.tech/WMA?sslmode=require",
+    },
+  };
+  
